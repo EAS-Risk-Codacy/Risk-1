fba35870f2d7c07f57366cae030279adffa5b782
andrecupa <andrecupa@gmail.com>
2022-04-14 12:30:06 +0200

    prueba Andrea ok8

--- test/ControllerTests/CheaterStrategyTest.java
+++ test/ControllerTests/CheaterStrategyTest.java
@@ -33,7 +33,8 @@ public class CheaterStrategyTest {
 	Country country2;
 	Country country3;
 	Country country4;
-	Country country5, country6, country7;
+	Country country5;
+	Country country6, country7;
 	Continent continent1, continent2;
 	HashMap<String, Country> temp;
 	HashMap<Integer, Player> temp1;
