cb73aac1ca8512e5f52a0d245b2ef825258a6110
andrecupa <andrecupa@gmail.com>
2022-04-18 12:52:45 +0200

    Forification arreglada modularización mantenimiento

--- test/controller_tests/FortificationTest.java
+++ test/controller_tests/FortificationTest.java
@@ -3,6 +3,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -34,46 +35,13 @@ public class FortificationTest {
 	Country countryEgypt;
 	Continent continentAsia;
 	Continent continentAfrica;
-
-	private void setCoutryGeneral(Country country, int idContinent, int idCountry, String name){
-		country.setContinentId(idContinent);
-		country.setCountryId(idCountry);
-		country.setName(name);
-	}
-
-	private ArrayList<Country> iniListcountries(Country[] countries){
-		List<Country> list = new ArrayList<Country>();
-		for(int i = 0; i < countries.length; i++){
-			list.add(countries[i]);
-		}
-		return (ArrayList<Country>) list;
-	}
-
-	private void iniPlayer(Player player, int id, String name, int armies, List<Country> totalCountriesOccupied){
-		player.setPlayerId(id);
-		player.setPlayerName(name);
-		player.setPlayerArmies(armies);
-		player.setTotalCountriesOccupied(totalCountriesOccupied);
-	}
-
-	private void setCountryspecific(Country country, List<Country> neighbors, int noOfArmies, Player player, Continent continent){
-		country.setNeighbors(neighbors);
-		country.setNoOfArmies(noOfArmies);
-		country.setPlayer(player);
-		country.setContinent(continent);
-	}
-
-	private void setContinenspecific(Continent continent, int id, String name, List<Country> coutries, int controlValue){
-		continent.setContinentId(id);
-		continent.setName(name);
-		continent.setCountries(coutries);
-		continent.setControlValue(controlValue);
-	}
 	/**
 	 * Method to create all objects
 	 */
 	@Before
 	public void onStart() {
+		OnStart start = new OnStart();
+
 		fortification = new FortificationController();
 		player1 = new Player(2);
 		countryIndia = new Country("India");
@@ -86,39 +54,45 @@ public class FortificationTest {
 		continentAsia = new Continent(4, "Asia");
 		continentAfrica = new Continent(5, "Africa");
 
-		setCoutryGeneral(countryIndia, 1, 11, "India");
-		setCoutryGeneral(countryChina, 2, 21, "China");
-		setCoutryGeneral(countryPakistan, 3, 31, "Pakistan");
-		setCoutryGeneral(countryBhutan, 4, 41, "Bhutan");
-		setCoutryGeneral(countryIran, 5, 51, "Iran");
-		setCoutryGeneral(countryCanada, 6, 61, "Canada");
-		setCoutryGeneral(countryEgypt, 7, 71, "Egypt");
+		start.setCountryData(countryIndia, 1, 11);
+		start.setCountryData(countryChina, 2, 21);
+		start.setCountryData(countryPakistan, 3, 31);
+		start.setCountryData(countryBhutan, 4, 41);
+		start.setCountryData(countryIran, 5, 51);
+		start.setCountryData(countryCanada, 6, 61);
+		start.setCountryData(countryEgypt, 7, 71);
 
 		Country[] listCountries1 = {countryChina, countryIran, countryCanada, countryPakistan};
-		List<Country> n_list = iniListcountries(listCountries1);
+		List<Country> n_list = new ArrayList<>(Arrays.asList(listCountries1));
 		Country[] listCountries2 = {countryChina, countryIran, countryPakistan, countryIndia};
-		List<Country> n_list4 = iniListcountries(listCountries2);
+		List<Country> n_list4 = new ArrayList<>(Arrays.asList(listCountries2));
 		Country[] listCountries3 = {countryPakistan};
-		List<Country> n_list3 = iniListcountries(listCountries3);
+		List<Country> n_list3 = new ArrayList<>(Arrays.asList(listCountries3));
 		Country[] listCountries4 = {countryIndia, countryIran, countryCanada};
-		List<Country> n_list1 = iniListcountries(listCountries4);
+		List<Country> n_list1 = new ArrayList<>(Arrays.asList(listCountries4));
 		Country[] listCountries5 = {countryIndia, countryPakistan, countryIran, countryBhutan};
-		List<Country> n_list2 = iniListcountries(listCountries5);
+		List<Country> n_list2 = new ArrayList<>(Arrays.asList(listCountries5));
 
 		player1 = new Player(9);
-		iniPlayer(player1,9, "abc", 7, n_list4);
+		start.setPlayerData(player1,9, "abc", 7, n_list4);
 		player2 = new Player(10);
-		iniPlayer(player2,10, "xyz", 8, n_list1);
+		start.setPlayerData(player2,10, "xyz", 8, n_list1);
 
-		setCountryspecific(countryIndia, n_list, 1, player1, continentAsia);
-		setCountryspecific(countryChina, n_list3, 4, player1, continentAsia);
-		setCountryspecific(countryPakistan, n_list, 4, player1, continentAsia);
-		setCountryspecific(countryCanada, n_list, 4, player1, continentAfrica);
-		setCountryspecific(countryIran, n_list3, 4, player2, continentAfrica);
-		setCountryspecific(countryBhutan, n_list1, 2, player1, continentAfrica);
+		start.setCountrySpecificData(countryIndia, n_list, 1, player1);
+		countryIndia.setContinent(continentAsia);
+		start.setCountrySpecificData(countryChina, n_list3, 4, player1);
+		countryChina.setContinent(continentAsia);
+		start.setCountrySpecificData(countryPakistan, n_list, 4, player1);
+		countryPakistan.setContinent(continentAsia);
+		start.setCountrySpecificData(countryCanada, n_list, 4, player1);
+		countryCanada.setContinent(continentAfrica);
+		start.setCountrySpecificData(countryIran, n_list3, 4, player2);
+		countryIran.setContinent(continentAfrica);
+		start.setCountrySpecificData(countryBhutan, n_list1, 2, player1);
+		countryBhutan.setContinent(continentAfrica);
 
-		setContinenspecific(continentAsia, 81, "Asia", n_list, 4);
-		setContinenspecific(continentAfrica, 82, "Africa", n_list2, 5);
+		start.setContinentData(continentAsia, 81, "Asia", n_list, 4);
+		start.setContinentData(continentAfrica, 82, "Africa", n_list2, 5);
 
 		ReadingFiles.ContinentNameObject = new HashMap<>();
 		ReadingFiles.ContinentNameObject.put(continentAsia.getName(), continentAsia);
